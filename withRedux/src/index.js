82367d1573d4f2f846edd0bab2f940327e2b7472
Vikram Belde <belvikram@gmail.com>
2019-12-25 13:08:08 -0600

    add title

--- withRedux/src/index.js
+++ withRedux/src/index.js
@@ -7,7 +7,7 @@ import reducer from './store/reducer';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({trace: true}));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
